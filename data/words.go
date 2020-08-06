fd9569eb23f7b09573d17555a99abd542e6f2d9c
Tommy Chu <tommychu2256@gmail.com>
2020-08-06 12:51:12 +0200

    add Service's contructor and Init/Stop methods

---
+++ data/words.go
@@ -0,0 +1,59 @@
+package data
+
+import (
+	"database/sql"
+	"fmt"
+
+	"github.com/pkg/errors"
+)
+
+// Service provides all actions with the database.
+type Service struct {
+	db *sql.DB
+}
+
+// ConfigDB holds database connection's credentials.
+type ConfigDB struct {
+	Host     string
+	Port     int
+	User     string
+	Password string
+	DBname   string
+}
+
+// New is a Service's constructor.
+func New() *Service {
+	return &Service{}
+}
+
+// Init initialize the database connection.
+func (s *Service) Init(cfg ConfigDB) error {
+
+	// define database connection
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", cfg.Host, cfg.Port, cfg.User, cfg.Password, cfg.DBname)
+
+	// connect to db
+	var err error
+	s.db, err = sql.Open("postgres", psqlInfo)
+	if err != nil {
+		return errors.Wrap(err, "connecting to the database")
+	}
+
+	// check the db connecton
+	err = s.db.Ping()
+	if err != nil {
+		return errors.Wrap(err, "ping to DB conn")
+	}
+
+	return nil
+}
+
+// Stop closes the database connection.
+func (s *Service) Stop() error {
+
+	// closure
+	if err := s.db.Close(); err != nil {
+		return errors.Wrap(err, "failed to close the database connection")
+	}
+	return nil
+}
