2898e31e1237da47e0266f4133f881fa14b45c26
Tommy Chu <tommychu2256@gmail.com>
2020-08-07 15:59:30 +0200

    add json tags

--- models/password.go
+++ models/password.go
@@ -2,14 +2,14 @@ package models
 
 // PasswordReq defines the password's request.
 type PasswordReq struct {
-	Length    int
-	ExtraNums int
-	ExtraSpec int
-	Helper    *string
+	Length    int     `json:"length"`
+	ExtraNums int     `json:"extra_nums"`
+	ExtraSpec int     `json:"extra_spec"`
+	Helper    *string `json:"helper"`
 }
 
 // PasswordResp defines the password's' response.
 type PasswordResp struct {
-	Passwd string
-	Helper string
+	Passwd string `json:"passwd"`
+	Helper string `json:"helper"`
 }
