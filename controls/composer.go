6019ae0a84ae67a98352942e79aff3fc9a0d0e50
Tommy Chu <tommychu2256@gmail.com>
2020-08-07 12:44:43 +0200

    complete the compose function

--- controls/composer.go
+++ controls/composer.go
@@ -1,9 +1,39 @@
 package controls
 
-		// getPhrase returns the phrase of the slice of the word.
-func (c *Controller) getPhrase([]word) (string, error) {
+import (
+	"strings"
 
-// composeWords composes the slice of words into the one string
-func (c *Controller) composeWords([]word) (string, error) {
+	"github.com/pkg/errors"
+)
 
+// composeWords composes the slice of words into the one string and returns it + the spaced phrase.
+func (c *Controller) composeWords(ws []word) (string, string, error) {
+
+	// define the phrase
+	var phrase []string
+
+	// range over the structures and generates the words for them
+	for _, w := range ws {
+
+		// get the random index
+		capacity, err := c.ds.Len(w.length, w.category)
+		if err != nil {
+			return "", "", errors.Wrap(err, "count the max length")
+		}
+		randI := c.rng.Intn(capacity)
+
+		// generate the word
+		newW, err := c.ds.Gen(w.length, w.category, randI)
+		if err != nil {
+			return "", "", errors.Wrap(err, "generating random word")
+		}
+
+		// add to the result
+		phrase = append(phrase, newW)
+	}
+
+	// join the phrase
+	smashed := strings.Join(phrase, "")
+	spaced := strings.Join(phrase, " ")
+	return smashed, spaced, nil
 }
