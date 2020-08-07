a4ec37338a82c03903c1cdb8ac2c031374d948f7
Tommy Chu <tommychu2256@gmail.com>
2020-08-07 17:31:22 +0200

    add PasswordGen reqquest handler

--- handlers/passwords.go
+++ handlers/passwords.go
@@ -1,20 +1,26 @@
 package handlers
 
 import (
+	"net/http"
+
 	config "github.com/chutified/smart-passwd/config"
 	controls "github.com/chutified/smart-passwd/controls"
+	"github.com/chutified/smart-passwd/models"
 	"github.com/gin-gonic/gin"
 	"github.com/pkg/errors"
 )
 
+// PWDhandler handles the backend generation.
 type PWDhandler struct {
 	pwdCtrl controls.Controller
 }
 
+// NewPWD is the constructor of the PWDhandler.
 func NewPWD() *PWDhandler {
 	return &PWDhandler{}
 }
 
+// Init starts the controller's services.
 func (h *PWDhandler) Init(cfg config.DBConfig) error {
 
 	// init the controller
@@ -26,6 +32,7 @@ func (h *PWDhandler) Init(cfg config.DBConfig) error {
 	return nil
 }
 
+// Close stops all connections.
 func (h *PWDhandler) Close() error {
 	//stop the controller
 	err := h.pwdCtrl.Stop()
@@ -36,4 +43,23 @@ func (h *PWDhandler) Close() error {
 	return nil
 }
 
-func (h *PWDhandler) Gen(c *gin.Context) {}
+// PasswordGen handles the password generation.
+func (h *PWDhandler) PasswordGen(c *gin.Context) {
+
+	// bind JSON
+	var preq models.PasswordReq
+	if err := c.ShouldBindJSON(&preq); err != nil {
+		c.JSON(http.StatusBadRequest,
+			gin.H{"error": err.Error()})
+	}
+
+	// generate the passwd
+	resp, err := h.pwdCtrl.Generate(&preq)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError,
+			gin.H{"error": err.Error()})
+	}
+
+	// response
+	c.JSON(200, resp)
+}
