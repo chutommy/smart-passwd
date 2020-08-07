d0761e6aa3f0225733309a249c07500452d523dd
Tommy Chu <tommychu2256@gmail.com>
2020-08-07 15:13:50 +0200

    complete the Generate function

--- controls/gen.go
+++ controls/gen.go
@@ -1,8 +1,47 @@
 package controls
 
-import "fmt"
+import (
+	"strings"
 
-func (c *Controller) Gen(l int) {
+	models "github.com/chutified/smart-passwd/models"
+	"github.com/pkg/errors"
+)
 
-	fmt.Println(c.newPhrase(l))
+// Generate handles the password's request and generates the password,
+// which satisfies the requirements.
+func (c *Controller) Generate(preq *models.PasswordReq) (*models.PasswordResp, error) {
+
+	var phrase string
+	var helper string
+	// get phrase
+	if preq.Helper == nil {
+
+		// generate words
+		ws, err := c.newPhrase(preq.Length)
+		if err != nil {
+			return nil, errors.Wrap(err, "generating new phrase")
+		}
+
+		// handle words
+		phrase, helper, err = c.composeWords(ws)
+		if err != nil {
+			return nil, errors.Wrap(err, "composing generated words")
+		}
+
+	} else {
+		phrase = strings.Replace(*preq.Helper, " ", "", -1)
+		helper = *preq.Helper
+	}
+
+	// transform the password
+	phrase = c.transform(phrase)
+	phrase = c.randomAdds(phrase, preq.ExtraNums, preq.ExtraSpec)
+
+	// assing into the response
+	presp := &models.PasswordResp{
+		Passwd: phrase,
+		Helper: helper,
+	}
+
+	return presp, nil
 }
