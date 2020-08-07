84015b84e4dd6a6cc08820f55071654faf739d5e
Tommy Chu <tommychu2256@gmail.com>
2020-08-07 12:13:28 +0200

    add template of the phrase copmposer

---
+++ controls/composer.go
@@ -0,0 +1,9 @@
+package controls
+
+		// getPhrase returns the phrase of the slice of the word.
+func (c *Controller) getPhrase([]word) (string, error) {
+
+// composeWords composes the slice of words into the one string
+func (c *Controller) composeWords([]word) (string, error) {
+
+}
