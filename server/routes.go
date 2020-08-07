bbe402770601d0012d9a7324ca2902f5d1ab0b26
Tommy Chu <tommychu2256@gmail.com>
2020-08-07 17:59:34 +0200

    set server's routing

---
+++ server/routes.go
@@ -0,0 +1,21 @@
+package server
+
+import (
+	handlers "github.com/chutified/smart-passwd/handlers"
+	"github.com/gin-gonic/gin"
+)
+
+// SetRoutes sets all routing for the gin's engine.
+func SetRoutes(e *gin.Engine, ph *handlers.PWDhandler) {
+
+	// ping to test server status
+	e.GET("/ping", func(c *gin.Context) {
+		c.String(200, "pong")
+	})
+
+	// set API
+	api := e.Group("/api")
+	{
+		api.GET("/passwd", ph.PasswordGen)
+	}
+}
