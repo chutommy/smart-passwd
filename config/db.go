d48eaffd1cb205c3985a7d77749712824a24463d
Tommy Chu <tommychu2256@gmail.com>
2020-08-07 15:42:37 +0200

    add yaml tags

--- config/db.go
+++ config/db.go
@@ -2,9 +2,9 @@ package config
 
 // DBConfig holds database connection's credentials.
 type DBConfig struct {
-	Host     string
-	Port     int
-	User     string
-	Password string
-	DBname   string
+	Host     string `yaml:"Host"`
+	Port     int    `yaml:"Port"`
+	User     string `yaml:"User"`
+	Password string `yaml:"Password"`
+	DBname   string `yaml:"Database_Name"`
 }
