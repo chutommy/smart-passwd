0eb5e96681c888d79f7de13acbbf7b330904f6e6
Tommy Chu <tommychu2256@gmail.com>
2020-08-07 18:00:12 +0200

    define the steps to run the server (api only)

--- main.go
+++ main.go
@@ -1,28 +1,34 @@
 package main
 
 import (
-	"fmt"
 	"log"
-	"time"
 
-	controls "github.com/chutified/smart-passwd/controls"
+	config "github.com/chutified/smart-passwd/config"
+	server "github.com/chutified/smart-passwd/server"
 	_ "github.com/lib/pq"
 )
 
 func main() {
-	t0 := time.Now()
 
-	c := controls.New()
-	err := c.Init()
+	// get configuration
+	cfg, err := config.GetConfig("config.yaml")
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer c.Stop()
 
-	err = c.Gen(5)
+	// set server
+	srv := server.New()
+	err = srv.Set(cfg)
 	if err != nil {
-		fmt.Printf("err = %+v\n", err)
+		log.Fatal(err)
 	}
 
-	fmt.Println(time.Since(t0))
+	// run server
+	err = srv.Start()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func() {
+		log.Panic(srv.Stop())
+	}()
 }
