75ba73e39bc53f80a7b9022147b275f3b96fb881
Tommy Chu <tommychu2256@gmail.com>
2020-08-07 14:01:55 +0200

    update list

--- swaps/lists.go
+++ swaps/lists.go
@@ -8,14 +8,18 @@ var (
 	// num holds the list of the numer characters.
 	num = []rune{'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'}
 	// special holds valid specials characters.
-	special = []rune{'!', '@', '#', '$', '%', '&', '*', '?'}
+	special = []rune{'!', '@', '#', '$', '%', '&', '*', '?', '+'}
 
 	// swap is the hash table of the swappable characters (look or sound similarly).
 	swap = map[rune][]rune{
 		'a': []rune{'@', '&', '4'},
-		'b': []rune{'6', '&'},
+		'b': []rune{'6', '&', 'G', 'd'},
+		'c': []rune{'e'},
+		'd': []rune{'b'},
+		'e': []rune{'c'},
+		'g': []rune{'9'},
 		'i': []rune{'j', 'l', '1', '!'},
-		'j': []rune{'i', '!'},
+		'j': []rune{'i', '!', '1'},
 		'l': []rune{'I', '!'},
 		'o': []rune{'O', '0', '@'},
 		'p': []rune{'q'},
@@ -23,10 +27,18 @@ var (
 		's': []rune{'$'},
 		'v': []rune{'w'},
 		'w': []rune{'v'},
+		'x': []rune{'+'},
+		'y': []rune{'i'},
+
+		'A': []rune{'4', '@', '&'},
 		'B': []rune{'8', '%'},
-		'I': []rune{'l', 'l'},
-		'O': []rune{'o', '0', '@'},
+		'D': []rune{'0', 'O'},
+		'G': []rune{'6'},
+		'I': []rune{'l', '1', '!'},
+		'O': []rune{'o', '0', '@', 'D'},
+		'T': []rune{'7'},
 		'V': []rune{'W'},
 		'W': []rune{'V'},
+		'X': []rune{'+'},
 	}
 )
