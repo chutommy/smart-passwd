e4afdb15cbfcf9298766d542ae6bdf71c6adb6ed
Tommy Chu <tommychu2256@gmail.com>
2020-08-09 09:39:53 +0200

    enable coppying the results

---
+++ templates/assets/js/copy.js
@@ -0,0 +1,10 @@
+function copyFunc(id) {
+    var text = document.getElementById(id).value;
+
+    // copy
+    navigator.clipboard.writeText(text).then(function() {
+        console.log('Async: Copying to clipboard was successful!');
+    }, function(err) {
+        console.error('Async: Could not copy text: ', err);
+    });
+}
