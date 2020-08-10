19b77acc285171148757d6cb91ada84692139bb6
Tommy Chu <tommychu2256@gmail.com>
2020-08-10 14:32:43 +0200

    add labels

--- templates/assets/js/copy.js
+++ templates/assets/js/copy.js
@@ -3,8 +3,8 @@ function copyFunc(id) {
 
     // copy
     navigator.clipboard.writeText(text).then(function() {
-        console.log('Async: Copying to clipboard was successful!');
+        $.notify("Successfully copied", "success");
     }, function(err) {
-        console.error('Async: Could not copy text: ', err);
+        $.notify("Unsuccessfully copied", "error");
     });
 }
