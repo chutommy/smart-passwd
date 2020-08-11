5512f12ed1ee300c1b60cb71785690a49945f0fe
Tommy Chu <tommychu2256@gmail.com>
2020-08-11 09:36:38 +0200

    add test for the config package

--- config/config.go
+++ config/config.go
@@ -10,10 +10,10 @@ import (
 )
 
 // ErrFileNotFound is returned if the file was not found
-type ErrFileNotFound error
+var ErrFileNotFound = errors.New("file config.yml was not found")
 
 // ErrInvalidYamlFile is return when the file does not satisfy the yaml file type.
-type ErrInvalidYamlFile error
+var ErrInvalidYamlFile = errors.New("file config.yaml has invalid content")
 
 // Config defines a configuration for the web API.
 type Config struct {
@@ -27,14 +27,14 @@ func GetConfig(path string) (*Config, error) {
 	// load configuration
 	content, err := ioutil.ReadFile(filepath.Join(rootDir(), "..", path))
 	if err != nil {
-		return nil, ErrFileNotFound(errors.Wrap(err, "could not read config file"))
+		return nil, ErrFileNotFound
 	}
 
 	// validate and apply the settings
 	var cfg Config
 	err = yaml.Unmarshal(content, &cfg)
 	if err != nil {
-		return nil, ErrInvalidYamlFile(errors.Wrap(err, "invalid config file"))
+		return nil, ErrInvalidYamlFile
 	}
 
 	return &cfg, nil
---
+++ config/config_test.go
@@ -0,0 +1,40 @@
+package config
+
+import (
+	"testing"
+
+	"gopkg.in/go-playground/assert.v1"
+)
+
+func TestConfig(t *testing.T) {
+
+	tests := []struct {
+		name string
+		file string
+		err  error
+	}{
+		{
+			name: "success",
+			file: "config/tests/config_ok.yml",
+			err:  nil,
+		},
+		{
+			name: "invalid",
+			file: "config/tests/config_invalid.yml",
+			err:  ErrInvalidYamlFile,
+		},
+		{
+			name: "not found",
+			file: "config/tests/config.yml",
+			err:  ErrFileNotFound,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t1 *testing.T) {
+
+			_, err := GetConfig(test.file)
+			assert.Equal(t1, err, test.err)
+		})
+	}
+}
