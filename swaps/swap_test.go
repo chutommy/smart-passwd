2b6f0c03e75401b19e0a1abadd679c128cf3ffca
Tommy Chu <tommychu2256@gmail.com>
2020-08-11 09:46:37 +0200

    add tests for the swap package

---
+++ swaps/swap_test.go
@@ -0,0 +1,24 @@
+package swaps
+
+import (
+	"testing"
+
+	"gopkg.in/go-playground/assert.v1"
+)
+
+func TestSwapEngine(t *testing.T) {
+
+	se := New()
+
+	r1 := se.GetAlpha(2)
+	r2 := se.GetAlphaCap(2)
+	r3 := se.GetNum(2)
+	r4 := se.GetSpecial(1000)
+	r5 := se.GetSubst('a', 1000)
+
+	assert.NotEqual(t, r1, 'a')
+	assert.NotEqual(t, r2, 'A')
+	assert.NotEqual(t, r3, '1')
+	assert.NotEqual(t, r4, 'a')
+	assert.NotEqual(t, r5, 'a')
+}
