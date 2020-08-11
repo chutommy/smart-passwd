40620bc7384acbc159a5a935a32244bd65519c58
Tommy Chu <tommychu2256@gmail.com>
2020-08-11 09:11:43 +0200

    add error types

--- config/config.go
+++ config/config.go
@@ -9,6 +9,12 @@ import (
 	"gopkg.in/yaml.v2"
 )
 
+// ErrFileNotFound is returned if the file was not found
+type ErrFileNotFound error
+
+// ErrInvalidYamlFile is return when the file does not satisfy the yaml file type.
+type ErrInvalidYamlFile error
+
 // Config defines a configuration for the web API.
 type Config struct {
 	Port     int       `yaml:"Port"`
@@ -21,14 +27,14 @@ func GetConfig(path string) (*Config, error) {
 	// load configuration
 	content, err := ioutil.ReadFile(filepath.Join(rootDir(), "..", path))
 	if err != nil {
-		return nil, errors.Wrap(err, "could not read config file")
+		return nil, ErrFileNotFound(errors.Wrap(err, "could not read config file"))
 	}
 
 	// validate and apply the settings
 	var cfg Config
 	err = yaml.Unmarshal(content, &cfg)
 	if err != nil {
-		return nil, errors.Wrap(err, "invalid config file")
+		return nil, ErrInvalidYamlFile(errors.Wrap(err, "invalid config file"))
 	}
 
 	return &cfg, nil
