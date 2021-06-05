3515b6184402f2fa0eec519184a1f9574e1c9b2c
chutified <tommychu2256@gmail.com>
2021-06-05 18:13:47 +0200

    Implement sqlite wasm main file

--- wasm/sqlite/main.go
+++ wasm/sqlite/main.go
@@ -1,5 +1,51 @@
 package main
 
-func main() {
+import (
+	"context"
+	"log"
+	"os"
+	"syscall/js"
 
+	engine "github.com/chutified/smart-passwd/pkg/enginelite"
+)
+
+func main() {
+	defer os.Exit(0)
+
+	js.Global().Set("distribute", js.FuncOf(distribute))
+	js.Global().Set("generate", js.FuncOf(generate))
+
+	c := make(chan struct{}, 0)
+	<-c
+}
+
+func generate(_ js.Value, p []js.Value) interface{} {
+	engi := engine.Init(engine.NewConstructor(3, 22), engine.NewSwapper())
+
+	resp, err := engi.Generate(context.Background(), engine.NewRequest(0, int16(p[0].Int()), p[1].String()))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	out := make([]interface{}, 2)
+	out[0] = resp.Password()
+	out[1] = resp.Helper()
+
+	return js.ValueOf(out)
+}
+
+func distribute(_ js.Value, p []js.Value) interface{} {
+	ctr := engine.NewConstructor(3, 22)
+
+	arr, err := ctr.Distribute(int16(p[0].Int()))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	out := make([]interface{}, len(arr))
+	for i, e := range arr {
+		out[i] = e
+	}
+
+	return js.ValueOf(out)
 }
