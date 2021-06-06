10d805d99e85a7b2938860e44383aeb871afad4c
chutified <tommychu2256@gmail.com>
2021-06-06 12:25:19 +0200

    Implement client side password generation

--- templates/scripts/api.js
+++ templates/scripts/api.js
@@ -1,9 +1,9 @@
-const URI = '/gen';
+// const URI = '/gen';
 
 let statusCounter = 0;
 
 // post a request to a server and show generated values
-function generatePasswd() {
+async function generatePasswd() {
   // retrieve document values
   const extra = parseInt($("#extra").text());
   const helper = document.getElementById("helper").value;
@@ -18,56 +18,73 @@ function generatePasswd() {
     return
   }
 
-  if (helper.trim() === "" && len < 5) {
-    resetHelper();
-    return
-  }
-
-  if (helper.trim() === "" && len < 5) {
-    resetHelper();
-    return
-  }
-
   const request = {
     len: len,
     extra: extra,
     helper: helper.trim()
   };
 
-  const param = {
-    headers: {
-      "Content-Type": "application/json"
-    },
-    method: "POST",
-    body: JSON.stringify(request)
-  };
+  let result = await password(request);
+  updatePasswdFields(result[0], result[1]);
 
-  fetch(URI, param)
-    .then(data => data.json())
-    .then(resp => {
-      document.getElementById("passwd").value = resp.password;
-      document.getElementById("helper-p").value = resp.helper;
+  // const param = {
+  //   headers: {
+  //     "Content-Type": "application/json"
+  //   },
+  //   method: "POST",
+  //   body: JSON.stringify(request)
+  // };
+  //
+  // fetch(URI, param)
+  //   .then(data => data.json())
+  //   .then(resp => {
+  //     updatePasswdFields(resp.password, resp.helper)
+  //   })
+  //   .catch(error => console.log(error))
+}
 
-      document.getElementById("hideButton").disabled = false;
-      document.getElementById("copyPasswd").disabled = false;
-      document.getElementById("copyHelper").disabled = false;
+async function password(request) {
+  const helper = await parseHelper(request);
+  return generate(request.extra, helper);
+}
 
-      // status
-      const hide = document.getElementById("hideButton");
-      hide.innerHTML = '<i class="fas fa-eye"></i>';
-      document.getElementById("passwd").type = "text";
+async function parseHelper(request) {
+  if (request.helper === "") {
+    let newHelper = [];
 
-      statusCounter++;
-      const temp = statusCounter;
+    let dist = distribute(request.len);
+    for (let i = 0; i < dist.length; i++) {
+      newHelper.push(await randomWord(dist[i]));
+    }
 
-      // set visibility timeout
-      setTimeout(function () {
-        if (temp === statusCounter) {
-          if (hide.innerHTML === '<i class="fas fa-eye"></i>') {
-            togglePasswordVisibility();
-          }
-        }
-      }, 3000);
-    })
-    .catch(error => console.log(error))
+    request.helper = newHelper.join(" ")
+  }
+
+  return request.helper
+}
+
+function updatePasswdFields(password, helper) {
+  document.getElementById("passwd").value = password;
+  document.getElementById("helper-p").value = helper;
+
+  document.getElementById("hideButton").disabled = false;
+  document.getElementById("copyPasswd").disabled = false;
+  document.getElementById("copyHelper").disabled = false;
+
+// status
+  const hide = document.getElementById("hideButton");
+  hide.innerHTML = '<i class="fas fa-eye"></i>';
+  document.getElementById("passwd").type = "text";
+
+  statusCounter++;
+  const temp = statusCounter;
+
+// set visibility timeout
+  setTimeout(function () {
+    if (temp === statusCounter) {
+      if (hide.innerHTML === '<i class="fas fa-eye"></i>') {
+        togglePasswordVisibility();
+      }
+    }
+  }, 3000);
 }
